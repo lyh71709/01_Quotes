32dd346c0224d45f2f6a60ccf88739b2fc59c4e3
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2022-08-09 13:28:56 +1200

    Author Pages part 1

--- content/author.php
+++ content/author.php
@@ -1,12 +1,23 @@
-<h2>All Results</h2>
-
 <?php
+
+if(!isset($_REQUEST['authorID']))
+{
+    header('Location: index.php');
+}
 
 $find_sql = "SELECT * FROM `quotes`
 JOIN author ON (`author`.`AuthorID` = `quotes`.`AuthorID`)
 ";
 $find_query = mysqli_query($dbconnect, $find_sql);
 $find_rs = mysqli_fetch_assoc($find_query);
+
+
+
+<h2>All Results</h2>
+
+<?php
+
+
 
 // Loop through results and display them
 do {
