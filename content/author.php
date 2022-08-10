ea7390f2896508b7d13281be95ee9cd62492f75f
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2022-08-11 11:05:32 +1200

    Author pages aprt 2 vid

--- content/author.php
+++ content/author.php
@@ -5,36 +5,53 @@ if(!isset($_REQUEST['authorID']))
     header('Location: index.php');
 }
 
+$author_to_find = $_REQUEST['authorID'];
+
 $find_sql = "SELECT * FROM `quotes`
 JOIN author ON (`author`.`AuthorID` = `quotes`.`AuthorID`)
+WHERE `quotes`.`AuthorID` = $author_to_find
 ";
 $find_query = mysqli_query($dbconnect, $find_sql);
 $find_rs = mysqli_fetch_assoc($find_query);
 
+$country1 = $find_rs['Country1_ID'];
+$country2 = $find_rs['Counrty2_ID'];
 
+$occupation1 = $find_rs['Career1_ID'];
+$occupation2 = $find_rs['Career2_ID'];
 
-<h2>All Results</h2>
+// get author name
+include("get_author.php");
 
+?>
+
+<br />>
+<div class="about">
+    <h2><?php echo $full_name ?> - About</h2>
+
+    <p><b>Born:</b> <?php echo $find_rs['Born']; ?> </p>
+
+    <p>
+        <?php
+        // show countries
+        country_job($dbconnect, $country1, $country2, "Country", "Countries", "country","Country_ID", "Country")
+        ?>
+    </p>
+</div> <!-- / about the author div -->
+
+<br />
 <?php
-
-
 
 // Loop through results and display them
 do {
 
     $quote = preg_replace('/[^A-Za-z0-9.,\s\'\-]/', ' ', $find_rs['Quote']);
 
-    include("get_author.php");
-
     ?>
 
 <div class="results">
     <p>
         <?php echo $quote; ?><br />
-        <!-- display author name -->
-        <a href="index.php?page=author&authorID=<?php echo $find_rs['AuthorID']; ?>">
-        <?php echo $full_name; ?>
-        </a>
     </p>
 
     <?php include("show_subjects.php"); ?>
--- functions.php
+++ functions.php
@@ -7,6 +7,37 @@ function test_input($data) {
 	return $data;
 }
 
+function country_job($dbconnect, $entity_1, $entity_2, $label_sg, $label_pl, $stable, $entity_ID, $entity_name)
+
+{
+    $all_entities = array($entity_1, $entity_2);
+    // loop through items and look up their values
+
+    // Counts # of countries that without ID zero
+    $num_entities = count(array_filter($all_entities));
+
+    if ($num_entities == 1)
+    {
+        echo "<b>".$label_sg."</b>: ";
+    }
+
+    else { echo "<b>".$label_pl."</b>: ";}
+
+    foreach ($all_entities as $entity) {
+        $entity_sql = "SELECT * FROM $table WHERE $entity_ID = $entity";
+        $entity_query = mysqli_query($dbconnect, $entity_sql);
+        $entity_rs = mysqli_fetch_assoc($entity_query);
+
+        if ($entity != 0) {
+            ?>
+
+            <span class="author_entity tag"><?php echo $entity_rs[$entity_name]; ?> </span> &nbsp;
+
+            <?php
+        }
+    }
+}
+
 
 // entity is subject, country, occupation etc
 function autocomplete_list($dbconnect, $item_sql, $entity)    
